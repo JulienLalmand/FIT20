378ce983e5f7c89d6fac32fae33622c61bb76042
thibitus <thibault.lesuisse@student.odisee.be>
2016-11-05 16:28:54 +0100

    Feature en step definitions

--- src/test/java/be/odisee/fit20/BeschikbaarhedenInstellenStepDefinitions.java
+++ src/test/java/be/odisee/fit20/BeschikbaarhedenInstellenStepDefinitions.java
@@ -2,9 +2,13 @@ package be.odisee.fit20;
 import cucumber.api.java.nl.Gegeven;
 import cucumber.api.java.nl.Als;
 import cucumber.api.java.nl.Dan;
+
+import spock.lang.*
 import cucumber.api.PendingException;
 
 public class BeschikbaarhedenInstellenStepDefinitions {
+	
+	
 
 	@Als("^een Trainer wordt toegevoegd met naam \"([^\"]*)\", voornaam \"([^\"]*)\", accountnaam \"([^\"]*)\", email 'hans@vdb\\.com', rol \"([^\"]*)\", wachtwoord \"([^\"]*)\", status \"([^\"]*)\" ;$")
 	public void een_Trainer_wordt_toegevoegd_met_naam_voornaam_accountnaam_email_hans_vdb_com_rol_wachtwoord_status(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) throws Throwable {
@@ -25,6 +29,52 @@ public class BeschikbaarhedenInstellenStepDefinitions {
 	}
 
 
-	
+	@Dan("^\"([^\"]*)\" komt voor in de lijst van de personen opgeslaan in de database$")
+	public void komt_voor_in_de_lijst_van_de_personen_opgeslaan_in_de_database(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Als("^\"([^\"]*)\" een beschikbaarheid toevoegd op \"([^\"]*)\"$")
+	public void een_beschikbaarheid_toevoegd_op(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Als("^nog een beschikbaarheid toevoegd op \"([^\"]*)\"$")
+	public void nog_een_beschikbaarheid_toevoegd_op(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Dan("^komt de beschikbaarheid \"([^\"]*)\" en \"([^\"]*)\" voor in de lijst die wordt opgevraagd$")
+	public void komt_de_beschikbaarheid_en_voor_in_de_lijst_die_wordt_opgevraagd(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Als("^een Fitnesser wordt toegevoegd met naam \"([^\"]*)\", voornaam \"([^\"]*)\", accountnaam \"([^\"]*)\", email 'jan@db\\.com', rol \"([^\"]*)\", wachtwoord \"([^\"]*)\", status \"([^\"]*)\" ;$")
+	public void een_Fitnesser_wordt_toegevoegd_met_naam_voornaam_accountnaam_email_jan_db_com_rol_wachtwoord_status(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Dan("^komt \"([^\"]*)\" voor in de lijst van fitnessers$")
+	public void komt_voor_in_de_lijst_van_fitnessers(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Als("^\"([^\"]*)\" het trainingsslot \"([^\"]*)\" selecteert$")
+	public void het_trainingsslot_selecteert(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Dan("^wordt een trainingsslot in naam van \"([^\"]*)\" aangemaakt met \"([^\"]*)\" als trainer, met programma \"([^\"]*)\", Status \"([^\"]*)\", op  \"([^\"]*)\", met klanttevredenheid \"([^\"]*)\"$")
+	public void wordt_een_trainingsslot_in_naam_van_aangemaakt_met_als_trainer_met_programma_Status_op_met_klanttevredenheid(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 	
 }
--- src/test/resources/beschikbaarheid.feature
+++ src/test/resources/beschikbaarheid.feature
@@ -7,4 +7,17 @@ Scenario: Er wordt een trainer aangemaakt
 Als een Trainer wordt toegevoegd met naam "VDB", voornaam "Hans", accountnaam "hansvdb", email 'hans@vdb.com', rol "Trainer", wachtwoord "TestenIsGeweldig123", status "in gebruik" ;
 Dan komt "hansvdb" voor in de lijst van trainers
 En kan "hansvdb" inloggen met het wachtwoord "TestenIsGeweldig123"
+En "hansvdb" komt voor in de lijst van de personen opgeslaan in de database 
 
+Scenario: Een trainer voegt zijn beschikbaarheden toe en zorgt dat deze worden opgeslaan
+Als "hansvdb" een beschikbaarheid toevoegd op "26/11/2016 14:00"
+En nog een beschikbaarheid toevoegd op "31/11/2016 15:30"
+Dan komt de beschikbaarheid "26/11/2016 14:00" en "31/11/2016 15:30" voor in de lijst die wordt opgevraagd
+
+Scenario: Een fitnesser wordt aangemaakt en bekijkt de lijst met beschikbaarheden
+Als een Fitnesser wordt toegevoegd met naam "De brabant", voornaam "Jan", accountnaam "jandb", email 'jan@db.com', rol "Fitnesser", wachtwoord "TestenIsGeweldig123", status "in gebruik" ;
+Dan komt "jandb" voor in de lijst van fitnessers
+
+Scenario: Een fitnesser selecteert een trainingsslot 
+Als "jandb" het trainingsslot "31/11/2016 15:30" selecteert
+Dan wordt een trainingsslot in naam van "jandb" aangemaakt met "hansvdb" als trainer, met programma "Sport", Status "Aangemaakt", op  "31/11/2016 15:30", met klanttevredenheid "1000%"
